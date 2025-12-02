e60b861586227ad7c83a4e95feb69b2fbee91faf
Ansh Koshti <koshtiwork26@gmail.com>
2025-12-02 18:45:43 +0530

    Timer function added.

---
+++ scenes/scripts/hud.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+signal start_game
+
+
+func update_score(value):
+	$MarginContainer/Score.text = str(value)
+
+
+func update_time(value):
+	$MarginContainer/Time.text = str(value)
+
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	$Message.hide()
