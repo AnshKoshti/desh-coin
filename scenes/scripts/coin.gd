2dbeec74e85c78b2c84eba877fd4f95e61f7d525
Ansh Koshti <koshtiwork26@gmail.com>
2025-12-03 19:02:40 +0530

    Bug crash fix

--- scenes/scripts/coin.gd
+++ scenes/scripts/coin.gd
@@ -4,7 +4,7 @@ var screensize = Vector2.ZERO
 
 
 func pickup():
-	$CollisionShape2d.set_deferred("disabled", true)
+	$CollisionShape2D.set_deferred("disabled", true)
 	var tw = create_tween().set_parallel().set_trans(Tween.TRANS_QUAD)
 	tw.tween_property(self, "scale", scale * 3, 0.3)
 	tw.tween_property(self, "modulate:a", 0.0, 0.3)
